c771ca95226e230d74c3498e7d968dc264976ff7
alejop2007 <wifranet3@gmail.com>
2021-06-29 21:02:34 -0500

    second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,7 +1,36 @@
 
 
 public class App {
-    public static void main(String[] args) {
+
+    private String nombre;
+    private int edad;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+    public static void main(String[] args)throws Exception {
        
+           System.out.println("hello, World");
+            String res= "Hola"
+           int resultado= 15+32;
+           int resltado3=125 -32;
+           double resultado2= 25/3;
+           float resultado4 = 58*21;
+           int resultado5 = 21 % 9;
+           //operadores unarios
+           resultado++;//suma uno a la variable
+           resultado--;//resta una variable
+           resultado=-resultado
+           resultado=+resultado
+           //operadores logicos
+           respuesta = 5 > 8;
+           respuesta = 5 < 8;
+           respuesta = 5 <= 8;
+           respuesta = 5 >= 8;
+           respuesta = 5 != 8;
+           respuesta = 5 == 8;
+           respuesta = 12 < 5 && 4>1;
+           respuesta = 14 < 25 || 1>5
+           //operadores bit a bit
+           
+       }
     }
-}
